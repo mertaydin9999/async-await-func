a6248554919e32604aba4a4e48e69cc1a45fff68
mertaydin9999 <mertaydin9999@hotmail.com>
2023-09-23 16:37:04 +0300

    Proje tamamlandi

---
+++ app.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const getData = async (number) => {
+  let userId = "";
+  try {
+    if (typeof number === "number") {
+      userId = number;
+      const [userResponse, postResponse] = await Promise.all([
+        axios(`https://jsonplaceholder.typicode.com/users/${userId}`),
+        axios(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`),
+      ]);
+
+      const userData = userResponse.data;
+      const postData = postResponse.data;
+      console.log("User Data:", userData);
+      console.log("Post Data:", postData);
+    } else {
+      throw new Error("number bir sayi degil");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default getData;
